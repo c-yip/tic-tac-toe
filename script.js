908e6f92054430b8b3dcec8345e782daee58ccaf
Christian <christianyip92@gmail.com>
2022-08-17 15:28:45 -0700

    Add player variables and functions that assign inputs and classes to divs.

--- script.js
+++ script.js
@@ -9,20 +9,44 @@ const gameBoardModule = (() => {
     };
 })();
 
+// players factory function
+const player = (className, input) => {
+    return {className, input};
+}
+
 // game logic module
 const gameLogic = (() => {
-    const playerInput = function() {
-        console.log('text')
+    const playerOne = player("playerOne", "X");
+    const playerTwo = player("playerTwo", "O");
+    
+    let playerTwoTurn = false;
+
+    const playerInput = function(e) {
+        const cell = e.target;
+        console.log(playerTwoTurn)
+        // checks whether player two turn and assigns player to current class
+        const currentClass = function() {
+            if (playerTwoTurn == false) {
+                return playerOne;
+            } else return playerTwo;
+        }
+        
+        playerSelection(cell, currentClass());
     };
 
+    // adds class and input to div
+    const playerSelection = function(cell, currentClass) {
+        cell.classList.add(currentClass.className);
+        cell.textContent = currentClass.input;
+    }
+
     return {
-        playerInput
+        playerInput,
+        playerOne,
+        playerTwo
     };
 })();
-// players factory function
-const Player = (playerOne, playerTwo) => {
-    
-}
+
 
 // event listener
 gameBoardModule.boardCells.forEach(cell => {

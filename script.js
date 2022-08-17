4b149a5b43bb5e9ab2cc1e15f39fe76aca07edf9
Christian <christianyip92@gmail.com>
2022-08-17 14:14:14 -0700

    Add function and event listener to handle player input.

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ const gameBoardModule = (() => {
     return {
         gameBoard,
         boardCells
-    }
+    };
 })();
 
 // game logic module
+const gameLogic = (() => {
+    const playerInput = function() {
+        console.log('text')
+    };
 
+    return {
+        playerInput
+    };
+})();
 // players factory function
 const Player = (playerOne, playerTwo) => {
     
 }
+
+// event listener
+gameBoardModule.boardCells.forEach(cell => {
+    cell.addEventListener('click', gameLogic.playerInput, {once: true})
+})

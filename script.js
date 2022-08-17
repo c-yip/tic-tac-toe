6ba7f73e6813a7e0022f0badbc0b61da10b80ef1
Christian <christianyip92@gmail.com>
2022-08-17 12:18:49 -0700

    Change game board to be created via html instead of js.

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-const body = document.querySelector('body');
 // game board module
 const gameBoard = (() => {
-    const gameBoardContainer = document.querySelector('.game-board-container');
-    const generateGameBoard = () => {
-        for (i=0; i<9; i++) {
-            boardCell = document.createElement('div');
-            boardCell.classList.add('boardCell');
-            gameBoardContainer.append(boardCell);
-        }
-    }
-    return {
-        generateGameBoard
-    }
+    
 })();
-body.onload = gameBoard.generateGameBoard;
-
 
 // display controller module
 
